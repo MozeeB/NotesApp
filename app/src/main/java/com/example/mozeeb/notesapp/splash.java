bd8a9f4f42dd8d828c1d77412356971b85afc33c
mujiburrohman <mujibnewbie5@gmail.com>
2018-10-18 09:18:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mozeeb/notesapp/splash.java
@@ -0,0 +1,31 @@
+package com.example.mozeeb.notesapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mozeeb.notesapp.view.MainActivity;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //SplashScreen
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000); //waktu pending 3 detik
+                }catch (InterruptedException ex){
+                    ex.printStackTrace();
+                }finally {
+                    startActivity(new Intent(splash.this, MainActivity.class));
+                    finish();
+                }
+            }
+        });
+        thread.start();
+    }
+}
